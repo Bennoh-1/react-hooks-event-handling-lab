ac1442670ea584405e08e12dca01e15528b1a4fb
francis <franciskibira017@gmail.com>
2022-07-24 09:02:05 +0300

    handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+function Keypad(){
+    function formSubmit(e){
+        e.preventDefault();
+    }
+    function keyChange(){
+        console.log(`Entering password...`);
+    }
     return (
-        <div></div>
+        <div>
+            <form onSubmit={formSubmit}>
+                <input name="" type="password" onChange={keyChange}/>
+            </form>
+        </div>
     )
 }
 
